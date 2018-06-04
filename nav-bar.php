68385a6fa9c999b0ed5a53e8c6a13e0e1d7c1fbe
ArnettMegan <animemegie@aol.com>
2018-06-04 14:17:26 -0600

    moving folders

---
+++ nav-bar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-image" href="home.php">
+                <img src = "/res/img/lotus.jpg"></img>
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="anime.php">Anime</a>
+                </li>
+                <li>
+                    <a href="books.php">Books</a>
+                </li>
+                <li>
+                    <a href="movies.php">Movies</a>
+                </li>
+                <li>
+                    <a href="shows.php">TV Shows</a>
+                </li>
+
+        </div>
+    </div>
+</nav>
